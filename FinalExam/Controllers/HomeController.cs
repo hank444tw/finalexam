b47ffb1c805cfef0b8b792330f9b5f3aab8e0b09
hank <40441124@gm.nfu.edu.tw>
2018-12-26 13:56:01 +0800

    index檢視頁面,Member資料表,登入登出功能,Layout切換

---
+++ FinalExam/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalExam.Models; //導入Model模型
+
+namespace FinalExam.Controllers
+{ 
+    public class HomeController : Controller
+    {
+    DBFinalExamEntities db = new DBFinalExamEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signin(string Mem_id,string Mem_password,string Name)
+        {
+            var member = db.Member.Where(m => m.Mem_id == Mem_id && m.Mem_password == Mem_password).FirstOrDefault();
+            if(member == null)
+            {
+                ViewBag.Message = "帳號或密碼錯誤";
+                return View();
+            }
+            Session["Welcome"] = member.Name + " 你好!";  
+            Session["Member"] = member;         //_ViewStart判斷Layout切換
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Signout()
+        {
+            Session["Member"] = null;
+            return RedirectToAction("Index");
+        }
+    }
+}
